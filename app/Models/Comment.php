f3bca48dcb88e0e1189aaaab54d41311f933a5f3
JackyTanio <jackytanio66@gmail.com>
2024-12-01 15:49:54 +0700

    ya

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
+
 use Illuminate\Database\Eloquent\Model;
 
 namespace App\Models;
 
-=======
->>>>>>> test
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,26 +15,25 @@ class Comment extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
+
     protected $fillable = [
         'user_id',
         'forum_id',
         'body',
     ];
-=======
-    protected $fillable = ['user_id', 'thread_id', 'parent_comment_id', 'body'];
->>>>>>> test
+
+
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-<<<<<<< HEAD
+
     public function forum()
     {
         return $this->belongsTo(Forum::class);
-=======
+    }
     public function thread()
     {
         return $this->belongsTo(Thread::class);
@@ -62,6 +60,5 @@ class Comment extends Model
     public function downvoteCount()
     {
         return $this->upvotes()->where('vote_type', 'downvote')->count();
->>>>>>> test
     }
 }
