a8a8abe9b2e9ab43e55029845d32433d522610c7
JackyTanio <166985139+JackyTanio@users.noreply.github.com>
2024-12-13 20:49:57 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        if(config('app.env') === 'production') {
-                URL::forceScheme('https');
-        }
+        // if(config('app.env') === 'production') {
+        //         URL::forceScheme('https');
+        // }
     }
 }
